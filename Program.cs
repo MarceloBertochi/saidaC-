498c5b7f34b91e43e2b91127aea36950b7075c4a
MarceloBertochi <paidasamia@gmail.com>
2023-04-07 17:33:25 -0300

    estudo de saída do console

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SaidaCcharp
+
+{
+    class Program
+
+    {
+        static void Main (string[] args)
+
+        {
+            Console.Clear(); // limpa a tela, igual ao cls do prompt
+            Console.WriteLine("Hello Word!");
+            Console.WriteLine("Estou aprendendo C#");
+            Console.WriteLine("Isso é Maravilhoso");
+            Console.WriteLine(3 + 7);
+            /* A diferença do write para o writeLine é que o primeiro nâo
+                pula linha e o segundo pula uma linha depois do texto*/
+            Console.Write("Olá Mundo ");
+            Console.Write("Estou adorando o C#");
+
+        }
+    }
+}
